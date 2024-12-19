76f7b64101c578e534611020f09779856c568961
sasiska <vzuravlev20@gmail.com>
2024-12-20 01:05:22 +0300

    edits

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World! This is the third version!'
+    return 'Hello, World! This is the 4 version!'
 
 
 if __name__ == '__main__':
